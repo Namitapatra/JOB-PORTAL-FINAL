b6a1cb1717afcdc2cb4bfd34e5ef8a1a2ba8a51d
Namitapatra <namitapatra0505@gmail.com>
2024-11-08 00:38:48 +0530

    Create UpdateJobServlet.java

---
+++ src/main/java/com/servlet/UpdateJobServlet.java
@@ -0,0 +1,31 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/update")
+public class UpdateJobServlet extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			int id=Integer.parseInt(req.getParameter("id"))
+			String title=req.getParameter("title");
+			String location=req.getParameter("location");
+			String category=req.getParameter("category");
+			String status=req.getParameter("status");
+			String description=req.getParameter("description");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	
+}
