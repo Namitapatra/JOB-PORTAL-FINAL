60daa8a5ac548cc6215c8fdb5b6acfb2fb36175b
Namitapatra <namitapatra0505@gmail.com>
2024-11-08 01:48:22 +0530

    Update DeleteJobServlet.java

--- src/main/java/com/servlet/DeleteJobServlet.java
+++ src/main/java/com/servlet/DeleteJobServlet.java
@@ -1,5 +1,24 @@
+
 package com.servlet;
 
-public class DeleteJobServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DeleteJobServlet  extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			int id=Integer.parseInt(req.getParameter("id"));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
 }
