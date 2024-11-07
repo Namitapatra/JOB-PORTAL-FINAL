8776aa6419df69936ca9b144074649fa9d7944d2
Namitapatra <namitapatra0505@gmail.com>
2024-11-07 10:35:08 +0530

    Update JobDAO.java

--- src/main/java/com/dao/JobDAO.java
+++ src/main/java/com/dao/JobDAO.java
@@ -56,7 +56,14 @@ public class JobDAO {
 			while(rs.next())
 			{
 				j=new Jobs();
-				j.
+				j.setId(rs.getInt(1));
+				j.setTitle(rs.getString(2));
+				j.setLocation(rs.getString(3));
+				j.setCatagory(rs.getString(4));
+				j.setStatus(rs.getString(5));
+				j.setDescription(rs.getString(6));
+				j.setPdata(rs.getTimestamp(7)+"");
+				list.add(j);
 			}
 			
 		} catch (Exception e) {
